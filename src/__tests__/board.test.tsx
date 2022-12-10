cc440f55b4c7b29c673a7be918859a21e13bb2c0
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-12-10 19:23:33 +0100

    Update node version

--- src/__tests__/board.test.tsx
+++ src/__tests__/board.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as BrowserRouter } from 'react-router-dom'
 import { ProSidebarProvider } from 'react-pro-sidebar'
 import Board from '../pages/Board'
 
 it('renders placeholder board', () => {
   render(
     <ProSidebarProvider>
-      <Router>
+      <BrowserRouter>
         <Board />
-      </Router>
+      </BrowserRouter>
     </ProSidebarProvider>,
   )
   expect(screen.getByText('Board Content Goes Here')).toBeInTheDocument()
