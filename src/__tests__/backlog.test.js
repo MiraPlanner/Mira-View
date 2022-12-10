66c84ba4d501a50b5e4411b5e6641093cfc2ed03
Milan Koster van Groos <58393068+milankoster@users.noreply.github.com>
2022-12-10 19:27:48 +0100

    Fix jest tests (#23)
    * update board test
    
    * Fix test
    
    * install jest
    
    * set jest script
    
    * Fix Jest test
    
    * Update node version
    
    * Update node version on merge
    
    Co-authored-by: Milan <58393068+Desoxyr@users.noreply.github.com>

--- src/__tests__/backlog.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from '../root/App'
-
-it('renders placeholder backlog', () => {
-  render(<App />)
-  expect(screen.getByText('Backlog Content Goes Here')).toBeInTheDocument()
-})
---
+++ src/__tests__/board.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter as BrowserRouter } from 'react-router-dom'
+import { ProSidebarProvider } from 'react-pro-sidebar'
+import Board from '../pages/Board'
+
+it('renders placeholder board', () => {
+  render(
+    <ProSidebarProvider>
+      <BrowserRouter>
+        <Board />
+      </BrowserRouter>
+    </ProSidebarProvider>,
+  )
+  expect(screen.getByText('Board Content Goes Here')).toBeInTheDocument()
+})
