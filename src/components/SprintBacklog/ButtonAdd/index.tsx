d0f4507a6df1846d0f4d58b5dd395f7ca6541a43
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-12-10 15:50:13 +0100

    start of adding api connection for creating a new sprint

--- src/components/SprintBacklog/ButtonAdd/index.tsx
+++ src/components/SprintBacklog/ButtonAdd/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './ButtonAdd.module.scss'
 
 const ButtonAdd = () => {
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    makeSprint()
+  }, [])
+
+  const makeSprint = () => {
+    createSprint().then((response) => {
+      if (response.error) {
+        setError(true)
+      } else {
+        setError(false)
+        const createdSprint = response.response
+        const sortedSprints = fetchedSprints.sort((a, b) => a.name.localeCompare(b.name))
+        setSprints(sortedSprints)
+      }
+    })
+  }
+
   return <div className={styles.Enabled}>Create Sprint</div>
 }
 
