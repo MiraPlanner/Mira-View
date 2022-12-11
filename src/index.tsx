f135bb266016f30670ffd6b8e339f514cee15adf
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-12-11 23:42:45 +0100

    Rename more components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './root/App'
+import App from './root/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
