81f2a59523f16245affbdd4495fcaa71e7b47418
Milan <58393068+Desoxyr@users.noreply.github.com>
2023-01-14 16:44:24 +0100

    Add e2e for boards page and component test for sidenav

---
+++ cypress/e2e/board.cy.ts
@@ -0,0 +1,22 @@
+describe('The Boards Page', () => {
+  it('loads the page', () => {
+    cy.visit('http://localhost:3000/boards')
+    cy.url().should('include', '/boards')
+  })
+
+  it('contains the sidebar', () => {
+    cy.visit('http://localhost:3000/boards')
+    cy.get('*[class^="Header_Container"]').contains('Create')
+    cy.get('*[class^="Header_Container"]').find('img').should('be.visible')
+  })
+
+  it('contains the sidebar', () => {
+    cy.visit('http://localhost:3000/boards')
+    cy.get('*[class^="Sidenav_Container"]').get('*[class^="ps-menuitem-root"]*').contains('Backlog')
+    cy.get('*[class^="Sidenav_Container"]').get('*[class^="ps-menuitem-root"]*').contains('Board')
+    cy.get('*[class^="SidenavHeader_Container"]').contains('Milan Individual Project')
+    cy.get('*[class^="SidenavHeader_Container"]').contains('Software project')
+    cy.get('*[class^="SidenavHeader_ImageContainer"]').find('span').should('be.visible')
+    cy.get('*[class^="SidenavFooter_Container"]').contains("You're in a team-managed project")
+  })
+})
